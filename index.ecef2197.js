615b5e8b525858f722c0422b90d946b7ad51292f
verjane23 <verjane23.gmail.com>
2024-06-22 16:19:09 +0300

    make build

---
+++ index.ecef2197.js
@@ -0,0 +1,2 @@
+document.addEventListener("click",function(t){var e=document.querySelector(".spider"),c=document.querySelector(".wall"),i=c.clientWidth-e.clientWidth,n=c.clientHeight-e.clientHeight,l=t.offsetX-e.clientWidth/2,a=t.offsetY-e.clientHeight/2;e.style.top="".concat(Math.max(0,Math.min(n,a)),"px"),e.style.left="".concat(Math.max(0,Math.min(i,l)),"px")});
+//# sourceMappingURL=index.ecef2197.js.map
